676965ca1464bbdbd4f1b3cf732c6ed3a4381cb0
RafaelNarvaez <r-enc@hotmail.com>
2022-07-02 13:50:16 -0400

    adding projext files

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+require('@nomiclabs/hardhat-waffle');
+
+const privateKey = fs.readFileSync('.secret').toString().trim();
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [privateKey],
+    },
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/bed4fdcc76bb4978a9a3103ef0946f64',
+      accounts: [privateKey],
+    },
+  },
+  solidity: '0.8.4',
+};
---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+
+import { NFTProvider } from '../context/NFTContext';
+import { Footer, Navbar } from '../components';
+import '../styles/globals.css';
+
+const Marketplace = ({ Component, pageProps }) => (
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </div>
+
+      <Script src="https://kit.fontawesome.com/9e0bce5125.js" crossorigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
+);
+
+export default Marketplace;
